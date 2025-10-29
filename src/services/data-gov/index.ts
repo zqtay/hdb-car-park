d31f8c7c6ae6a13af8a69c121f7bbee9b242cf51
zqtay <zqtay96@gmail.com>
2025-10-29 14:13:09 +0800

    Use static json

--- src/services/data-gov/index.ts
+++ src/services/data-gov/index.ts
@@ -1,4 +1,6 @@
 import type { CarParkAvailabilityResponse, CarParkInfoResponse, PlanningAreaResponse, SubzoneBoundaryResponse } from "./types";
+import planningAreaJson from "./data/MasterPlan2019PlanningAreaBoundaryNoSea.geojson?url";
+import subzoneBoundary from "./data/MasterPlan2019SubzoneBoundaryNoSeaGEOJSON.geojson?url";
 
 const getCarParkInfo = async (offset?: number, limit?: number): Promise<CarParkInfoResponse> => {
   const datasetId = "d_23f946fa557947f93a8043bbef41dd09";
@@ -22,43 +24,13 @@ const getCarParkAvailability = async (time?: string): Promise<CarParkAvailabilit
 };
 
 const getPlanningArea = async (): Promise<PlanningAreaResponse> => {
-  const datasetId = "d_4765db0e87b9c86336792efe8a1f7a66";
-  const url = "https://api-open.data.gov.sg/v1/public/api/datasets/" + datasetId + "/poll-download";
-  let response = await fetch(url);
-  if (!response.ok) {
-    throw new Error('Failed to fetch poll-download data');
-  }
-  const jsonData = await response.json();
-  if (jsonData['code'] != 0) {
-    throw new Error(jsonData['errMsg']);
-  }
-
-  const fetchUrl = jsonData['data']['url'];
-  response = await fetch(fetchUrl);
-  if (!response.ok) {
-    throw new Error('Failed to fetch inner data');
-  }
+  const response = await fetch(planningAreaJson);
   const data = await response.json();
   return data;
 };
 
 const getSubzoneBoundary = async (): Promise<SubzoneBoundaryResponse> => {
-  const datasetId = "d_8594ae9ff96d0c708bc2af633048edfb"
-  const url = "https://api-open.data.gov.sg/v1/public/api/datasets/" + datasetId + "/poll-download";
-  let response = await fetch(url);
-  if (!response.ok) {
-    throw new Error('Failed to fetch poll-download data');
-  }
-  const jsonData = await response.json();
-  if (jsonData['code'] != 0) {
-    throw new Error(jsonData['errMsg']);
-  }
-
-  const fetchUrl = jsonData['data']['url'];
-  response = await fetch(fetchUrl);
-  if (!response.ok) {
-    throw new Error('Failed to fetch inner data');
-  }
+  const response = await fetch(subzoneBoundary);
   const data = await response.json();
   return data;
 };
